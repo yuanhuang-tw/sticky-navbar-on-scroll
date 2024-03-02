443064e67f930e361cf9de1fa2b62712d0d724dd
Yuan Huang@MBA <huangjenyuan@gmail.com>
2024-03-02 11:01:49 +0800

    update

--- app.js
+++ app.js
@@ -2,10 +2,6 @@ const logo = document.querySelector('.logo');
 const hamburgerMenu = document.querySelector('.hamburger-menu');
 const links = document.querySelector('.links');
 
-document.querySelector('html').style.scrollPaddingTop = `${
-  navHeight - navHeight / 3
-}px`;
-
 window.addEventListener('scroll', () => {
   if (window.scrollY > navHeight) {
     nav.classList.add('my-sticky-nav');
@@ -16,14 +12,12 @@ window.addEventListener('scroll', () => {
     logo.classList.remove('my-sticky-logo');
     links.classList.remove('my-sticky-links');
   }
-
-  links.style.top = `${nav.clientHeight}px`;
 });
 
 hamburgerMenu.addEventListener('click', () => {
   hamburgerMenu.classList.toggle('open');
 
-  links.style.top = `${nav.clientHeight}px`;
+  links.style.top = `${nav.offsetHeight}px`;
   links.classList.toggle('active');
 });
 
@@ -31,5 +25,13 @@ links.addEventListener('click', (e) => {
   if (e.target.tagName === 'A') {
     hamburgerMenu.classList.remove('open');
     links.classList.remove('active');
+  }
+});
+
+window.addEventListener('resize', () => {
+  if (window.innerWidth < 1024) {
+    links.style.top = `${nav.offsetHeight}px`;
+  } else {
+    links.style.top = '0px';
   }
 });
